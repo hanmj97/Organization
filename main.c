da246c0ae1a16d0c77f8c7031c49929643ae367a
ChangjunPark2 <38196255+ChangjunPark2@users.noreply.github.com>
2018-04-11 21:37:38 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,12 @@
 #include<stdio.h>
-
+#include<stdlib.h>
+#include<time.h>
 int main()
 {
   int n;
+  
+  srand((unsigned)time(NULL));
+	int sr = rand() % 10 + 1;
   
   printf("Team project\n");
   printf("숫자를 입력해주세요.\n");
@@ -12,6 +16,19 @@ int main()
   for(int a=1 ; a < 10 ; a++){
     printf(" %d x %d = %d\n",n,a,n*a);
   }
+  
+  int c = n * sr;
+	int an;
+	printf("구구단 문제!\n %d X %d = ", n, sr);
+
+	scanf("%d", &an);
+
+	if (an == c) {
+		printf("정답입니다");
+	}
+	else if (an != c) {
+		printf("땡 정답은 %d 입니다.\n", c);
+	}
   
   return 0;
 }
